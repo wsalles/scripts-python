8f1b368d4ec4dc6a125691cbe92f7dcb11975384
WALLACE ROBINSON LOUZA SALLES <wallace_robinson@hotmail.com>
2019-06-27 19:45:13 +0000

    disk_space.py edited online with Bitbucket

--- disk_space.py
+++ disk_space.py
@@ -1,8 +1,13 @@
 #-*- coding: utf-8 -*-
 import sys, os
 
-stat = os.statvfs("MOUNT")
-total = (stat.f_bsize * stat.f_blocks) / 1024
-avail = (stat.f_bsize * stat.f_bavail) / 1024
-used = total-avail
-percent = float(used)/float(total)*100
+def percentDisk(path):
+        stat = os.statvfs(path)
+        total = (stat.f_bsize * stat.f_blocks) / 1024
+        avail = (stat.f_bsize * stat.f_bavail) / 1024
+        used = total-avail
+        percent = float(used)/float(total)*100
+        p = ("%.2f" % percent)
+        return p
+
+mount_point = percentDisk("/mnt/exemplo")
