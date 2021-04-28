ca4ddf4774582a1de5deabbd9fde784b2ca4a8e7
wallace.salles <wallace.salles@g.globo>
2021-04-27 23:38:08 -0300

    revision: deleted useless files

--- testes/testeArgv.py
+++
@@ -1,9 +0,0 @@
-#-*- coding: utf-8 -*-
-import os, sys
-
-def argumentos(*argv):    
-	for arg in argv:
-		print(str(argv) + " - Argumento: " + arg)
-
-#argumentos(sys.argv[1], sys.argv[2], sys.argv[3])
-print(sys.argv[1])
